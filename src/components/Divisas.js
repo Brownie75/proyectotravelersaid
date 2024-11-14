0ba4d14ad5cc9fbf3cf7b8709a10c5a83c3ea82e
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2024-11-14 12:25:38 -0600

    cosas

--- src/components/Divisas.js
+++ src/components/Divisas.js
@@ -11,7 +11,7 @@ function IngresarDivisas(){
 
   async function Convert() {
     try {
-      const response = await fetch('https://v6.exchangerate-api.c8', {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/pair/EUR/GBP/AMOUNT`, {
         headers: {
           Authorization: 'Bearer c86266179d76808931135a54'
         }
@@ -27,11 +27,51 @@ function IngresarDivisas(){
     });
 
     return(
-        <div>
-          <h1 style={{marginTop:'200px', color: 'black'}}>Hola</h1>
-            <select onChange={handleChange}>
-              <option value = {'USD'}>Dolar</option>
+        <div style={{display:'flex', flex: 'row', marginTop: '8%'}}>
+          <div style={{marginLeft: '20%', display:'flex', flex: 'row'}}>
+            <input type='number'></input>
+
+          <select onChange={handleChange} name = "select_divisas" id="divisas" style={{marginLeft: '3%'}}>
+              <option>Elija una divisa</option>
+              <option value = {'/USD'}>Dolar estadounidense</option>
+              <option value = {'/MXN'}>Peso mexicano</option>
+              <option value = {'/CAD'}>Dolar canadiense</option>
+              <option value = {'/AUD'}>Dolar australiano</option>
+              <option value = {'/JPY'}>Yen japones</option>
+              <option value = {'/EUR'}>Euro</option>
+              <option value = {'/GBP'}>Libra esterlina</option>
+              <option value = {'KRW'}>Won surcoreano</option>
+              <option value = {'/CNY'}>Yuan chino</option>
+              <option value = {'/COP'}>Peso colombaino</option>
+              <option value = {'/CLP'}>Peso chileno</option>
+              <option value = {'/ARS'}>Peso argentino</option>
+              <option value = {'/VES'}>Bolivar vanezolano</option>
             </select>
+            <div class= "">
+
+            </div>
+          </div>
+
+          <div style={{ marginLeft: '10%', display:'flex', flex: 'row'}}>
+            <input type='number'></input>
+
+          <select onChange={handleChange} name = "select_divisas" id="divisas" style={{marginLeft: '3%'}}>
+              <option>Elija una divisa</option>
+              <option value = {'/USD'}>Dolar estadounidense</option>
+              <option value = {'/MXN'}>Peso mexicano</option>
+              <option value = {'/CAD'}>Dolar canadiense</option>
+              <option value = {'/AUD'}>Dolar australiano</option>
+              <option value = {'/JPY'}>Yen japones</option>
+              <option value = {'/EUR'}>Euro</option>
+              <option value = {'/GBP'}>Libra esterlina</option>
+              <option value = {'KRW'}>Won surcoreano</option>
+              <option value = {'/CNY'}>Yuan chino</option>
+              <option value = {'/COP'}>Peso colombaino</option>
+              <option value = {'/CLP'}>Peso chileno</option>
+              <option value = {'/ARS'}>Peso argentino</option>
+              <option value = {'/VES'}>Bolivar vanezolano</option>
+            </select>
+          </div>
             
         </div>
     )
